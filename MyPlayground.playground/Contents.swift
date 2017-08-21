8f3b215e1aec971d2177bdcc1078407acf26f6e4
Sophia <sophshaw99@gmail.com>
2017-08-20 23:18:10 -0700

    lab0 First Attempt

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,37 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+let name = "Sophia Shaw"
+print ("\(str) \nHello, \(name)")
+
+//Data Types
+var day = "Friday"
+let value = 42
+let myPi = 3.14
+var numberOfStopLights: Int = 4
+var population: Int
+population = 5422
+let townName: String = "Knowhere"
+let townDescription = "\(townName) has population of \(population) and \(numberOfStopLights) stoplights."
+print(townDescription)
+
+//Arrays
+var arrayOfInts: [Int]  = [1, 2, 3, 4, 5]
+var names: [String] = ["Homer", "Marge", "Bart", "Lisa"]
+names.append("Maggie")
+
+//Initializers
+let emptyString = String()
+let defaultNumber = Int()
+let defaultBool = Bool()
+let number = 42
+let meaningOfLife = String(number)
+
+//Dictionaries
+let nameByParkingSpace = [13: "Alice", 27: "Bob"]
+let space13Name: String? = nameByParkingSpace[13]
+
+
+
